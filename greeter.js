b5c75ccb4c3de51028719556cdea28ab6bcc5cd1
zhujunliang <igt6469@dingtalk.com>
2020-09-28 17:22:18 +0800

    Typescript学习

--- greeter.js
+++
@@ -1,7 +0,0 @@
-var Color;
-(function (Color) {
-    Color["red"] = "\u7EA2";
-    Color["green"] = "\u7EFF";
-    Color["blue"] = "\u84DD";
-})(Color || (Color = {}));
-console.info(Color.red, Color.green);
